7d708cd68ad8df5bd41d66f80a1fb31259d11ca3
aakrut <aakrutdabhi@gmail.com>
2022-06-04 12:10:39 +0530

    add algorithms-prime-factors-js

---
+++ prime-factors.js
@@ -0,0 +1,15 @@
+const primeFactors = n => {
+    let a = [];
+    f=2;
+    while (n > 1) {
+        if (n % f === 0) {
+            a.push(f);
+            n /= f;
+        } else {
+            f++;
+        }
+    }
+    return a;
+}
+
+console.log(primeFactors(235));
