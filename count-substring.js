44a498b6485cc17bde52254fffd0d0fe1713f8cf
aakrut <aakrutdabhi@gmail.com>
2022-06-05 12:31:36 +0530

    add algorithms-count-substrings-js

---
+++ count-substring.js
@@ -0,0 +1,11 @@
+const countSubstrings = (str, searchValue) => {
+  let count = 0,
+    i = 0;
+  while (true) {
+    const r = str.indexOf(searchValue, i);
+    if (r !== -1) [count, i] = [count + 1, r + 1];
+    else return count;
+  }
+}
+
+console.log(countSubstrings("buky buck buck bucky buck buck", "buck"));
